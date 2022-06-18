bd2fb868460baa1174647f9f79e8af627d6a3392
fazazy123 <fazazygbolagade@gmail.com>
2022-06-18 15:28:27 +0100

    deploying my app

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,64 @@
+// When the user clicks on div, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
 //MENU SHOW
-const showMenu = (toggleId, navId) =>{
-    const toggle=document.getElementById(toggleId);
-    const nav=document.getElementById(navId)
-    console.log({toggle});
-    console.log({navId});
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    // console.log({ toggle });    
+    // console.log({ nav });
 
     if (toggle && nav){
-        toggle.addEventListener("click" ,()=>{
-            nav.classList.toggle("show");//javacsricpt css display nav menu
-        })
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')  //js csss to show the nav menu
+        });   
     }
-
 };
-showMenu("nav-toggle","nav-menu");
+showMenu('nav-toggle', 'nav-menu');
 
-/*=========================================ACTIVE AND REMOVE MENU MOBILE==================*/
-const navlink = document.querySelectorAll(".nav__link");
+//Active and remove menu mobile
+const navLink = document.querySelectorAll('.nav__link');
 
-console.log({navlink});
+console.log({ navLink });
 
-function linkAction(){
-    const navMenu = document.getElementById("nav-menu");
-    navMenu.classList.remove("show");//When we click on each nav__link,we remove the show class
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show'); //when we click on each nav__link, we remove the show class
 }
-navlink.forEach((n)=>n.addEventListener("click" , linkAction));
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 
-/*============SCROLL REVEAL ANIMATION==================*/
+// SCROLL REVIEW ANIMATION
 const sr = ScrollReveal({
-    origin:"top",
-    distance:"80",
-    duration:"2000",
-    reset:true,
+    origin: 'top',
+    distance:'80px',
+    duration: 2000,
+    //delay: 200,
+    reset: true
 })
+
 // SCROLL HOME
-sr.reveal(".home__title", {});
-sr.reveal(".button",{delay:200});
-sr.reveal(".home__img",{delay:400});
-sr.reveal(".home__social-icon",{interval: 400});
+sr.reveal('.home__title', {});
+sr.reveal('.button', { delay: 200 });
+sr.reveal('.home__img', { delay: 400 });
+sr.reveal('.home__social-icon', { interval: 300 })
 
 //SCROLL ABOUT
-sr.reveal(".about__img", {});
-sr.reveal(".about__subtitle",{delay:200});
-sr.reveal(".about__text",{delay:400});
+sr.reveal('.about__img', {});
+sr.reveal('.about__subtitle', { delay: 200 });
+sr.reveal('.about__text', { delay: 400 });
 
-//SCROOLL SKILLS
-sr.reveal(".skills__sutitle", {});
-sr.reveal(".skills__text",{delay:200});
-sr.reveal(".skill__data",{delay:400});
-sr.reveal(".skills__img",{interval: 400});
+// SCROLL SKILLS
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__text',  { delay: 200 });
+sr.reveal('.skills__data', { interval: 400 });
+sr.reveal('.skills__img', { delay: 400 });
 
-//SCROLL WORK
-sr.reveal(".work__img",{interval:200});
+// SCROLL WORK
+sr.reveal('.work__img', { interval: 200 });
 
-//SCROLL CONTACT
-sr.reveal(".contact__input",{interval:300});
+// SCROLL CONTACT
+sr.reveal('.contact__input', {interval: 300});
